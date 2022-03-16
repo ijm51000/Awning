06029c96caa2492f0d291746eded159732735f4f
imac <imac@openanswers.co.uk>
2022-03-16 14:07:38 +0000

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,12 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
  
-const char* ssid = "mac-wifi"; // Enter your WiFi name
-const char* password =  ""; // Enter WiFi password
+const char* ssid = "themacs"; // Enter your WiFi name
+const char* password =  "macmac51"; // Enter WiFi password
 const char* mqttServer = "192.168.0.130";
 const int mqttPort = 1883;
 const char* mqttUser = "ian";
-const char* mqttPassword = "";
+const char* mqttPassword = "macmac51";
  
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -15,6 +15,10 @@ void callback(char* topic, byte* payload, unsigned int length) {
  
   Serial.print("Message arrived in topic: ");
   Serial.println(topic);
+  if (topic == "awning/setTargetPosition") {
+    Serial.println(topic);
+    client.publish("awning/CurrentPosition", "100")
+  }
  
   Serial.print("Message:");
   for (unsigned int i = 0; i < length; i++) {
@@ -25,6 +29,8 @@ void callback(char* topic, byte* payload, unsigned int length) {
   Serial.println("-----------------------");
  
 }
+
+
 void setup() {
  
   Serial.begin(115200);
@@ -39,6 +45,7 @@ void setup() {
  
   client.setServer(mqttServer, mqttPort);
   client.setCallback(callback);
+//  client.setCallback(callback2)
  
   while (!client.connected()) {
     Serial.println("Connecting to MQTT...");
@@ -56,9 +63,9 @@ void setup() {
     }
   }
  
-  client.publish("esp/test", "hello"); //Topic name
-  client.subscribe("esp/test");
- 
+  client.publish("awning/CurrentPosition", "0"); //Topic name
+  client.subscribe("awning/setTargetPosition");
+  client.subscribe("awning/getPositionState");
 }
  
 
