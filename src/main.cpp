19506348459f9e60d247df39868ec16ab6f0aa58
ijm51000 <ianmac51@gmail.com>
2022-03-21 19:34:35 +0000

    commit by ian mac

--- src/main.cpp
+++ src/main.cpp
@@ -12,9 +12,16 @@ const char* mqttUser = "ian";
 const char* mqttPassword = "macmac51";
 
 
-void test(String topic){
-Serial.println("from inside the func");
-Serial.println(topic);
+unsigned int openAwning(void){
+  Serial.println("hello from open awningfunction");
+  delay(3000);
+  return 1;
+}
+
+unsigned int closeAwning(void){
+  Serial.println("hello from close awning function");
+  delay(3000);
+  return 1;
 }
 
 
@@ -23,47 +30,35 @@ PubSubClient client(espClient);
 void callback(char* topic, byte* payload, unsigned int length) {
   String message;
   String strTopic;
-  //int msg =0;
-  Serial.print("Message arrived in topic: ");
-  Serial.print(strTopic);
-  Serial.print("Message:");
   for (unsigned int i = 0; i < length; i++) {
-    //Serial.print((char)payload[i]);
     message += (char)payload[i];
   }
-  Serial.println(message);
   int msg = message.toInt();
-  Serial.println();
-  Serial.println("-----------------------");
   strTopic = String((char*)topic);
-  Serial.println("topic value");
-  Serial.println(message);
   int str_len = strTopic.length() + 1; 
   char char_array[str_len];
   strTopic.toCharArray(char_array, str_len);
   if (strTopic == "awning/setTargetPosition") {
     Serial.println(topic);
-
     if (msg) {
-     char blindposition[10];
-     test(strTopic);
-     int count = 10;
-     for (int i = 0; i < count; i++){
-      digitalWrite(LED, HIGH); //turn the led off
-      delay(1000); //wait for 1 sec
-      digitalWrite(LED, LOW); //turn the led on
-// delay(1000); //wait for 1 sec 
-
-//     delay(3000);
-     }
-     Serial.println("Setting awning open");
+      char blindposition[10];
+      client.publish("awning/getPositionState", "INCREASING");
+      int open = openAwning();
+      if (open) {
+        client.publish("awning/getPositionState", "STOPPED");
+        Serial.println("awning open");
+      }
+     //needs a char array so convert 
      itoa(msg, blindposition, 10);
      client.publish("awning/CurrentPosition", blindposition);
     }
     else {
-      Serial.println("Setting awning close");
-      Serial.println("sending: ");
-      Serial.println(msg);
+      client.publish("awning/getPositionState", "DECREASING");      
+      int closed = closeAwning();
+      if (closed) {
+      client.publish("awning/getPositionState", "STOPPED");  
+      Serial.println("awning closed");
+      }
       client.publish("awning/CurrentPosition", "0"); 
     }
   }
