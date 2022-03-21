476b0ecbd719a1766454246016878d4b45db0004
ijm51000 <ianmac51@gmail.com>
2022-03-21 21:56:24 +0000

    outline motor drive control

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
-#define LED D0 //Led in NodeMCU at pin GPIO16 (D0) 
-
+// IN1  IN2 Motor direction
+// 0    0  Stop
+// 0    1  clockwise
+// 1    0 clockwise
+// 1    1 Stop 
+int IN1 = D1; //Direction pins
+int IN2 = D2; 
 
 const char* ssid = "themacs"; // Enter your WiFi name
 const char* password =  "macmac51"; // Enter WiFi password
@@ -14,12 +19,22 @@ const char* mqttPassword = "macmac51";
 
 unsigned int openAwning(void){
   Serial.println("hello from open awningfunction");
+  // start motor to open
+  // this needs a while loop for the hall sensor 
+  // use delay for testin
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
   delay(3000);
   return 1;
 }
 
 unsigned int closeAwning(void){
   Serial.println("hello from close awning function");
+    // start motor to close
+  // this needs a while loop for the hall sensor 
+  // use delay for testin
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
   delay(3000);
   return 1;
 }
@@ -68,9 +83,11 @@ void callback(char* topic, byte* payload, unsigned int length) {
 
 
 void setup() {
- 
+  // motor direction pins output
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);  
   Serial.begin(115200);
-  pinMode(LED, OUTPUT); //LED pin as output
+//  pinMode(LED, OUTPUT); //LED pin as output
   WiFi.begin(ssid, password);
  
   while (WiFi.status() != WL_CONNECTED) {
